75619b8c184568f97fade694ee2bfe64e1eff237
alireza GumaryaN <alireza313gomarian@gmail.com>
2023-06-26 21:32:55 +0330

    add Pure_user interface in domain layer

---
+++ src/domain/model/Pure_user.model.ts
@@ -0,0 +1,10 @@
+export interface Pure_user {
+    first_name: string;
+    last_name: string;
+    email: string;
+    phone_number: string;
+    password: string;
+    address: string;
+    profile_image: string;
+    uuid: string;
+}
