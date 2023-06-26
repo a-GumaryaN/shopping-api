3b67a7cb555ce012387c52f628d33fab66d52e37
alireza GumaryaN <alireza313gomarian@gmail.com>
2023-06-26 21:33:15 +0330

    add Order  model in domain layer

---
+++ src/domain/model/Order.ts
@@ -0,0 +1,11 @@
+interface Order_product {
+  product_id: string;
+  number: number;
+}
+
+export interface Order {
+  order_products: Order_product[];
+  description: string;
+  total_amount: number;
+  order_uuid: string;
+}
